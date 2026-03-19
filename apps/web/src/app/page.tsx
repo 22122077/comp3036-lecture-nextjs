07fe1e2fff375b500f93ef4bc6be482aef93ebb4
Mina Armijo <22122077@student.westernsydney.edu.au>
2026-03-19 20:15:40 +1100

    Adding active post filter, set up title, description, category, and date posted

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,29 @@
 import styles from "./page.module.css";
+import { posts } from "@repo/db/data";
+import Link from "next/link";
 
 export default function Home() {
-  return <div className={styles.main}>Home</div>;
+
+  //filter active posts
+  const activePosts = posts.filter((p) => p.active);
+
+  return (
+    <main className = {styles.main}>
+      <h1>All Posts</h1>
+
+      <ul>
+        {activePosts.map((p) => (
+          <li key={p.id}>
+            <h2>
+              <Link href={`/posts/${p.urlId}`}>{p.title}</Link>
+            </h2>
+
+            <p>{p.description}</p>
+            <p>{p.category}</p>
+            <p>{p.date.toDateString()}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
